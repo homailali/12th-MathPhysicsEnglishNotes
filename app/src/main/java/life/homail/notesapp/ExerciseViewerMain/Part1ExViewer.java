5733b1f8647565102b6215f1677133b055fcfc37
homailali <138436251+homailali@users.noreply.github.com>
2024-02-17 18:06:06 +0500

    Some improvements

--- app/src/main/java/life/homail/notesapp/ExerciseViewerMain/Part1ExViewer.java
+++ app/src/main/java/life/homail/notesapp/ExerciseViewerMain/Part1ExViewer.java
@@ -27,6 +27,93 @@ public class Part1ExViewer {
                     case "8"-> this.setPdf("MathPdfs/Part1Math/Ch2/ex_2 (8).pdf");
                 }
             }
+            case "3"->{
+                switch (this.exerciseViewerMain.exerciseNo){
+                    case "1"-> this.setPdf("MathPdfs/Part1Math/Ch3/ex_3 (1).pdf");
+                    case "2"-> this.setPdf("MathPdfs/Part1Math/Ch3/ex_3 (2).pdf");
+                    case "3"-> this.setPdf("MathPdfs/Part1Math/Ch3/ex_3 (3).pdf");
+                    case "4"-> this.setPdf("MathPdfs/Part1Math/Ch3/ex_3 (4).pdf");
+                    case "5"-> this.setPdf("MathPdfs/Part1Math/Ch3/ex_3 (5).pdf");
+                }
+            }
+            case "4"->{
+                switch (this.exerciseViewerMain.exerciseNo){
+                    case "1"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (1).pdf");
+                    case "2"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (2).pdf");
+                    case "3"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (3).pdf");
+                    case "4"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (4).pdf");
+                    case "5"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (5).pdf");
+                    case "6"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (6).pdf");
+                    case "7"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (7).pdf");
+                    case "8"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (8).pdf");
+                    case "9"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (9).pdf");
+                    case "10"-> this.setPdf("MathPdfs/Part1Math/Ch4/ex_4 (10).pdf");
+                }
+            }
+            case "5"->{
+                switch (this.exerciseViewerMain.exerciseNo){
+                    case "1"-> this.setPdf("MathPdfs/Part1Math/Ch5/ex_5 (1).pdf");
+                    case "2"-> this.setPdf("MathPdfs/Part1Math/Ch5/ex_5 (2).pdf");
+                    case "3"-> this.setPdf("MathPdfs/Part1Math/Ch5/ex_5 (3).pdf");
+                    case "4"-> this.setPdf("MathPdfs/Part1Math/Ch5/ex_5 (4).pdf");
+                }
+            }
+            case "6"->{
+                switch (this.exerciseViewerMain.exerciseNo){
+                    case "1"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (1).pdf");
+                    case "2"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (2).pdf");
+                    case "3"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (3).pdf");
+                    case "4"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (4).pdf");
+                    case "5"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (5).pdf");
+                    case "6"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (6).pdf");
+                    case "7"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (7).pdf");
+                    case "8"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (8).pdf");
+                    case "9"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (9).pdf");
+                    case "10"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (10).pdf");
+                    case "11"-> this.setPdf("MathPdfs/Part1Math/Ch6/ex_6 (11).pdf");
+                }
+            }
+            case "7"->{
+                switch (this.exerciseViewerMain.exerciseNo){
+                    case "1"-> this.setPdf("MathPdfs/Part1Math/Ch7/ex_7 (1).pdf");
+                    case "2"-> this.setPdf("MathPdfs/Part1Math/Ch7/ex_7 (2).pdf");
+                    case "3"-> this.setPdf("MathPdfs/Part1Math/Ch7/ex_7 (3).pdf");
+                    case "4"-> this.setPdf("MathPdfs/Part1Math/Ch7/ex_7 (4).pdf");
+                    case "5"-> this.setPdf("MathPdfs/Part1Math/Ch7/ex_7 (5).pdf");
+                    case "6"-> this.setPdf("MathPdfs/Part1Math/Ch7/ex_7 (6).pdf");
+                    case "7"-> this.setPdf("MathPdfs/Part1Math/Ch7/ex_7 (7).pdf");
+                    case "8"-> this.setPdf("MathPdfs/Part1Math/Ch7/ex_7 (8).pdf");
+                }
+            }
+            case "8"->{
+                switch (this.exerciseViewerMain.exerciseNo){
+                    case "1"-> this.setPdf("MathPdfs/Part1Math/Ch8/ex_8 (1).pdf");
+                    case "2"-> this.setPdf("MathPdfs/Part1Math/Ch8/ex_8 (2).pdf");
+                    case "3"-> this.setPdf("MathPdfs/Part1Math/Ch8/ex_8 (3).pdf");
+                }
+            }
+            case "9"->{
+                switch (this.exerciseViewerMain.exerciseNo){
+                    case "1"-> this.setPdf("MathPdfs/Part1Math/Ch9/ex_9 (1).pdf");
+                    case "2"-> this.setPdf("MathPdfs/Part1Math/Ch9/ex_9 (2).pdf");
+                    case "3"-> this.setPdf("MathPdfs/Part1Math/Ch9/ex_9 (3).pdf");
+                    case "4"-> this.setPdf("MathPdfs/Part1Math/Ch9/ex_9 (4).pdf");
+                }
+            }
+            case "10"->{
+                switch (this.exerciseViewerMain.exerciseNo){
+                    case "1"-> this.setPdf("MathPdfs/Part1Math/Ch10/ex_10 (1).pdf");
+                    case "2"-> this.setPdf("MathPdfs/Part1Math/Ch10/ex_10 (2).pdf");
+                    case "3"-> this.setPdf("MathPdfs/Part1Math/Ch10/ex_10 (3).pdf");
+                    case "4"-> this.setPdf("MathPdfs/Part1Math/Ch10/ex_10 (4).pdf");
+                }
+            }
+            case "11"->{
+                switch (this.exerciseViewerMain.exerciseNo){
+                    case "1"-> this.setPdf("MathPdfs/Part1Math/Ch11/ex_11 (1).pdf");
+                    case "2"-> this.setPdf("MathPdfs/Part1Math/Ch11/ex_11 (2).pdf");
+                }
+            }
         }
     }
 
